2b0e8dbf638adffaf14e458f074fc6cb53592289
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-05 11:02:19 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,7 +47,7 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-  delete '/posts/:id/delete' do
+  delete '/posts/:id/delete' do #delete action
     @post = Post.find_by_id(params[:id])
     @post.delete
     erb :deleted
